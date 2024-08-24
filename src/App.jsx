c612b2fb8a106e7f4374e0a351bec8a41810193f
Franklin Aidoo <ph.aidoo.58@gmail.com>
2024-08-24 13:56:18 +0000

    Routing problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -67,15 +67,19 @@ function App() {
   return (
     <Router>
       <Routes>
+
+        {/* Authentication */}
         <Route 
           path="/" 
           element={<Login />} 
         />
+
+        {/* Home Route */}
         <Route 
           path="/home" 
           element={
             <div className='flex h-screen'>
-              {/* Sidebar */}
+              {/* Sidebar */}    
               <div className="h-full max-w-[20rem]">
                 <Sidebar />
               </div>
@@ -91,17 +95,129 @@ function App() {
                 <div className="flex-grow p-4 bg-slate-200 overflow-auto">
                   <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/applications" element={<Applications />} />
-                    <Route path="/jobs" element={<Jobs inputs={userInputs} title="Add new Entry" />} />
-                    <Route path="/reports" element={<Reports />} />
-                    <Route path="/settings" element={<Settings />} />
                   </Routes>
                 </div>
               </div>
             </div>
           } 
         />
+
+        {/* Job Route */}
+        <Route 
+          path="/jobs" 
+          element={
+            <div className='flex h-screen'>
+              {/* Sidebar */}    
+              <div className="h-full max-w-[20rem]">
+                <Sidebar />
+              </div>
+
+              {/* Main Content Section */}
+              <div className="flex flex-col flex-grow">
+                {/* Navbar */}
+                <div className="h-[calc(4rem)] w-full">
+                  <Navbar />
+                </div>
+
+                {/* Main Content */}
+                <div className="flex-grow p-4 bg-slate-200 overflow-auto">
+                  <Routes>
+                    <Route path="/" element={<Jobs inputs={userInputs} title="Add new Entry" />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          } 
+        />
+
+        {/* Report Route */}
+        <Route 
+          path="/reports" 
+          element={
+            <div className='flex h-screen'>
+              {/* Sidebar */}    
+              <div className="h-full max-w-[20rem]">
+                <Sidebar />
+              </div>
+
+              {/* Main Content Section */}
+              <div className="flex flex-col flex-grow">
+                {/* Navbar */}
+                <div className="h-[calc(4rem)] w-full">
+                  <Navbar />
+                </div>
+
+                {/* Main Content */}
+                <div className="flex-grow p-4 bg-slate-200 overflow-auto">
+                  <Routes>
+                    <Route path="/" element={<Reports />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          } 
+        />
+
+        {/* Applications Route */}
+        <Route 
+          path="/applications" 
+          element={
+            <div className='flex h-screen'>
+              {/* Sidebar */}    
+              <div className="h-full max-w-[20rem]">
+                <Sidebar />
+              </div>
+
+              {/* Main Content Section */}
+              <div className="flex flex-col flex-grow">
+                {/* Navbar */}
+                <div className="h-[calc(4rem)] w-full">
+                  <Navbar />
+                </div>
+
+                {/* Main Content */}
+                <div className="flex-grow p-4 bg-slate-200 overflow-auto">
+                  <Routes>
+                    <Route path="/" element={<Applications />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          } 
+        />
+
+        {/* Settings Route */}
+        <Route 
+          path="/settings" 
+          element={
+            <div className='flex h-screen'>
+              {/* Sidebar */}    
+              <div className="h-full max-w-[20rem]">
+                <Sidebar />
+              </div>
+
+              {/* Main Content Section */}
+              <div className="flex flex-col flex-grow">
+                {/* Navbar */}
+                <div className="h-[calc(4rem)] w-full">
+                  <Navbar />
+                </div>
+
+                {/* Main Content */}
+                <div className="flex-grow p-4 bg-slate-200 overflow-auto">
+                  <Routes>
+                  <Route path="/settings" element={<Settings />} />
+                  </Routes>
+                </div>
+              </div>
+            </div>
+          } 
+        />
+
+
+
       </Routes>
+
     </Router>
   );
 }
